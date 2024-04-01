4681987bcb5760cb727be3474dcc80b56f05ce02
Mouser5 <stanislavegorkin7@gmail.com>
2024-04-01 20:52:11 +0300

    Попытка сдать номер 1
    Удалил библиотеку, как как вызывалось много ошибок при чтении функций из библиотеки в классе

--- C++Dz2.cpp
+++ C++Dz2.cpp
@@ -2,32 +2,69 @@
 #include <vector>
 #include <string>
 #include "Header.h"
+#include <ctime>
 using namespace std;
 
 class Student {
 private:
     string name;
-    string mark;
+    char mark;
     vector <double> ans;
 public:
-    Student(string _name, string _mark) {
+    Student(const string _name,const int number) {
         name = _name;
-        mark = _mark;
-        ans=Get_ans(mark);
+        Get_Mark(mark);
+        Get_ans(ans,mark, number);
     }
-    vector <double> Get_ans(string mark) {
+    void Get_Mark( char& mark  ) {
+        srand(time(0));
+        int score = rand() % (3) + 3;
+        if (score == 3)
+            mark='C';
+        if (score == 4)
+            mark='B';
+        else
+            mark='A';
+    }
+    void Get_ans(vector <double> & ans,const char mark, const int number) {
         
-        if (mark == "A") {
-            
+        if (mark == 'A') {
+            FindRoots(ans, number);
+        }
+        if (mark == 'C') {
+            ans[0] = 0.0;
+        }
+        if (mark == 'B') {
+            int chance = rand() % (20) + 1;
+            if (chance <= 11)
+                FindRoots(ans, number);
+            else
+                ans[0] = 0.0;
+        }
+    }
+    void PrintResult(const int number) {
+        cout << "№ -" << number<< endl;
+        cout << name;
+        if (ans[0] == NULL) {
+            cout << "There are no roots.";
+        }
+        else
+            cout << ans[0];
+        if (ans[1] != NULL) {
+            cout << ans[1];
         }
     }
 };
 
 int main()
 {
-   // string s = "example";
-   // Student* a = new Student(s, s);
-    double a, b, c;
-    InputData(a,  b, c);
+    //номер уравнения - number
+    // Фамилия - name
+    // Ответ - ans
+    string name;
+    int number;
+    cin >> name >> number;
+    Student* a = new Student(name,number);
+    a->PrintResult(number);
 }
 
--- Header.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void Discriminant(const double a, const double b, const double c, double& D);
-
-void Roots(const double a, const double b, const double c, double D, double& x1, double& x2);
-
-void InputData(double& a, double& b, double& c);
