911131b65ce33c2844068902e32ccfb13bb58616
Mouser5 <stanislavegorkin7@gmail.com>
2024-04-01 19:17:18 +0300

    Def

---
+++ Header.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void Discriminant(const double a, const double b, const double c, double& D) {}
+
+void Roots(const double a, const double b, const double D, double& x1, double& x2) {};
